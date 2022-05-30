1d352f47bac6bf35dc6d49483c38146cec8a394a
Steven van Veldhoven <42697075+Steven2203@users.noreply.github.com>
2022-05-30 16:54:03 +0200

    Add files via upload

---
+++ module 8 octagon challenge.py
@@ -0,0 +1,43 @@
+#-------------------------------------------------------------------------------
+# Name:        module 8 - Understand repeating events.
+# Purpose:     Using the turtle module, draw an octagon.
+# Author:      Steven
+# Created:     26-05-2022
+# Copyright:   (c) Steven 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+##Importing the turtle module.
+import turtle as tl
+
+##Using the turtle functions available to draw an octagon manually.
+
+"""
+tl.color('blue')
+tl.right(45)
+tl.forward(30)
+tl.right(45)
+tl.forward(30)
+tl.right(45)
+tl.forward(30)
+tl.right(45)
+tl.forward(30)
+tl.right(45)
+tl.forward(30)
+tl.right(45)
+tl.forward(30)
+tl.right(45)
+tl.forward(30)
+tl.right(45)
+tl.forward(30)
+"""
+##Using a loop to print a single blue octagon.
+
+"""
+for steps in range(8):
+    tl.color('blue')
+    tl.forward(30)
+    tl.right(45)
+"""
+##Using a nested loop to print an extra object inside the octagon.
+
